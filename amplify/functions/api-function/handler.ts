376c48b7a0cb98b4d32362b8ef04303644cbe278
manoj <107401848+manojtatipamula@users.noreply.github.com>
2024-05-27 01:35:27 +0530

    Update handler.ts

--- amplify/functions/api-function/handler.ts
+++ amplify/functions/api-function/handler.ts
@@ -5,7 +5,8 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     return {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin": "*", // Restrict this to domains you trust
+
+     "Access-Control-Allow-Methods": "*",       "Access-Control-Allow-Origin": "*", // Restrict this to domains you trust
             "Access-Control-Allow-Headers": "*", // Specify only the headers you need to allow
         },
         body: JSON.stringify("Hello from manoj REST API")
